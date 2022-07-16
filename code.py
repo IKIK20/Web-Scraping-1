6e8676d83b7664d82a8ea08de0a54a7634893db3
IKIK20 <67692637+IKIK20@users.noreply.github.com>
2022-07-16 13:47:54 +0530

    Add files via upload

--- code.py
+++ code.py
@@ -14,25 +14,28 @@ def scrape():
     headers = ["Name", "Distance", "Mass", "Radius"]
     star_data = []
 
-    for i in range(0, 100):
-        soup= BeautifulSoup(browser.page_source, "html.parser")
+    soup= BeautifulSoup(browser.page_source, "html.parser")
         #temp_25_list = []
-        for ul_tag in soup.find_all("tr"):
-            td_tags= ul_tag.find_all("td")
-            temp_list = []
-            
-            for j, td_tag in enumerate(td_tags):
-                if j==0:
-                    temp_list.append(td_tag.find_all("a")[0].contents[0])
-                else:
-                    try:
-                        temp_list.append(td_tag.contents[0])
-                    except:
-                        temp_list.append("")
+    star_table = soup.find('table')
+    star_table.find_all('tr')
 
-            star_data.append(temp_list)
-        browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
-        verify= False
+    for ul_tag in soup.find_all("tr"): 
+        td_tags= ul_tag.find_all("td")
+        temp_list = []
+            
+        for j, td_tag in enumerate(td_tags):
+            if j==0:
+                temp_list.append(td_tag.find_all("a")[0].contents[0])
+            else:
+                try:
+                    temp_list.append(td_tag.contents[0])
+                except:
+                    temp_list.append("")
+
+    star_data.append(temp_list)
+    browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+    verify= False
+    
     
     with open("scraper.csv","w") as file:
         writer= csv.writer(file)
